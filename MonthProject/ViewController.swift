ba3e0956056b27b6ffe381c8967764f754f36899
Misael Pérez Chamorro <misaelpcgm@gmail.com>
2018-11-30 16:46:40 -0600

    Initial Commit

---
+++ MonthProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MonthProject
+//
+//  Created by Misael Pérez Chamorro on 11/30/18.
+//  Copyright © 2018 Misael Pérez Chamorro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
